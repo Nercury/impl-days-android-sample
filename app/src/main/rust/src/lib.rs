35338f5cf931f4d051b5ea8767d6a0675ec2ea36
Nerijus Arlauskas <nercury@gmail.com>
2018-05-29 10:48:58 +0200

    Carko extension path is retrievable

---
+++ app/src/main/rust/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate jni;
+
+use jni::JNIEnv;
+use jni::objects::JObject;
+use jni::sys::jstring;
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_com_example_hellojni_HelloJni_stringFromJNI(
+    env: JNIEnv,
+    _this: JObject
+) -> jstring {
+    env.new_string("Hello from Rust").expect("Could not create string").into_inner()
+}
